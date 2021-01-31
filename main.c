28232eee90a99e09d209717506d7f27814e61d41
Semih Artan <s-785412369@hotmail.com>
2021-01-31 14:40:26 +0300

    Add the documentation

--- main.c
+++ main.c
@@ -13,11 +13,11 @@ int main()
 { 
 	//"50 + 4 * 2 / 4 - 9 + 3 * 6 - 2 * 4 - 5 * 6 - 0"
 	char* expr = "50 + 4 * 2/ 4 - 9 + 3 * 6 * myfunc(5+6)";
-	ZParser parser = CreateParser(expr);
-	AddFunc(parser, "myfunc", myfunc);
-	ZNode root = ParseExpression(parser); 
-	double result = Evaluate(root);
-	printf("Result = %lf\n", result);
+	ZParser parser = CreateParser(expr); // Create a parser to read the expression
+	AddFunc(parser, "myfunc", myfunc); // Add custom function definition 
+	ZNode root = ParseExpression(parser);  // Get the root node of the expression tree. 
+	double result = Evaluate(root); // Finally, evaluate the tree starting from root.
+	printf("Result = %lf\n", result); // Print the result
 	getchar();
 	return 0;
 }
